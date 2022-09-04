03140e5d2d8672564481d4c4b8ef85eb7bb25285
Dio naufal vianto <69904821+DevPadd@users.noreply.github.com>
2022-09-04 21:28:21 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const nav = document.querySelector("header nav ul");
-hamburgerMenu.addEventListener("click", (e) => {
-    nav.classList.toggle("show");
-});
-nav.addEventListener("click", (e) => {
-    nav.classList.remove("show");
-});
